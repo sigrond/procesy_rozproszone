126b4dc87be661e80d0ee80790fd43599a456938
Tomasz Jakubczyk <sigrond93@gmail.com>
2015-12-15 16:17:40 +0100

    wstepne odbieranie komunikatów AdminServer :warning: nie kompilowane #10

--- application/server/AdminServer.cpp
+++ application/server/AdminServer.cpp
@@ -5,18 +5,81 @@
  */
 
 #include "AdminServer.hpp"
+#include <exception>
+#include <iostream>
 
-AdminServer::AdminServer()
+/** \brief Klasa opakowująca exception dla AdminServer
+ */
+struct AdminServerException : std::exception
 {
+private:
+	std::string m;
+public:
+    /** \brief Wygodny kostruktor wyjątku
+     * \param std::string s komunikat do wyświetlenia
+     */
+	AdminServerExceptionException(std::string s)
+	{
+		m="AdminServerException: "+s+"\n";
+	}
+    /** \brief przeciążona metoda wyświetla nasz komunikat
+     * \return const char* komunikat
+     */
+	const char* what() const noexcept
+	{
+		return m.c_str();
+	}
+};
 
+AdminServer::AdminServer() :
+	blockingQueue(nullptr),
+	connection(nullptr),
+	adminIP(nullptr)
+{
+	#ifdef _DEBUG
+	std::clog<<"AdminServer::AdminServer()\n";
+	#endif // _DEBUG
+	connection=new Connection();
 }
 
-void AdminServer::connect()
+AdminServer::AdminServer(Ip &ip) :
+	blockingQueue(nullptr),
+	connection(nullptr)
+	adminIP(nullptr)
 {
+	#ifdef _DEBUG
+	std::clog<<"AdminServer::AdminServer(Ip &ip)\n";
+	#endif // _DEBUG
+	connection=new Connection(ip);
+	adminIP=&ip;
+}
 
+AdminServer::~AdminServer()
+{
+	#ifdef _DEBUG
+	std::clog<<"AdminServer::~AdminServer()\n";
+	#endif // _DEBUG
+	if(connection!=nullptr)
+	{
+		delete connection;
+	}
+	if(adminIP!=nullptr)
+	{
+		delete adminIP;
+	}
 }
 
 void AdminServer::listen()
+{
+	#ifdef _DEBUG
+	std::clog<<"AdminServer::listen()\n";
+	#endif // _DEBUG
+	message::Message* m=nullptr;
+	m=connection->receive();
+	blockingQueue->push_back(new Event(MESSAGE_FROM_ADMIN_SERVER,m));
+}
+
+void AdminServer::connect()
 {
 
 }
--- application/server/AdminServer.hpp
+++ application/server/AdminServer.hpp
@@ -8,16 +8,32 @@
 
 #include "BlockingQueue.hpp"
 #include "Event.hpp"
+#include "../protocol/Connection.hpp"
+/**< \todo potrzebne implementacje Connection */
 
-/** \brief klasa serwera administratora
+/** \brief Klasa serwera administratora.
+ * Należy się spodziewać raczej asynchronicznego działania.
  */
 class AdminServer
 {
 public:
+    /** \brief Konstruktor bez podania adresu administratora.
+     * Oznacza to, że czekamy na pierwsze zapytanie z konsoli administratora,
+     * a jak już przyjdzie to przyjmujemy jego adres jako adres admina.
+     */
     AdminServer();
+    /** \brief Konstruktor z podaniem adresu administratora.
+     *
+     * \param ip Ip& Adres konsoli administratora.
+     *
+     */
+    AdminServer(Ip &ip);
+    ~AdminServer();
     void listen();/**< metoda nasłuchująca na połączenie z konsoli administratora */
     void connect();/**< metoda łącząca się z konsolą administratora */
     void setBlockingQueue(BlockingQueue<Event*>*);
 private:
     BlockingQueue<Event*>* blockingQueue;
+    Connection* connection;
+    Ip* adminIP;/**< opcjonalne IP admina */
 };
