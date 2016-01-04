1b131f7e58ec77f87e1f0fee98e3cfaeee15596a
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-01-04 19:28:50 +0100

    poprawiona obsługa pingowania w kontrolerze. #10

--- application/server/version.h
+++ application/server/version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 76;
-	static const long REVISION  = 453;
+	static const long BUILD  = 79;
+	static const long REVISION  = 472;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 318;
-	#define RC_FILEVERSION 0,1,76,453
-	#define RC_FILEVERSION_STRING "0, 1, 76, 453\0"
-	static const char FULLVERSION_STRING [] = "0.1.76.453";
+	static const long BUILDS_COUNT  = 322;
+	#define RC_FILEVERSION 0,1,79,472
+	#define RC_FILEVERSION_STRING "0, 1, 79, 472\0"
+	static const char FULLVERSION_STRING [] = "0.1.79.472";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 76;
+	static const long BUILD_HISTORY  = 79;
 	
 
 }
