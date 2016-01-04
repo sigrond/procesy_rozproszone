d9efba173f6f33a7b116346e69633aa3ce50d88f
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-01-04 20:36:56 +0100

    nowy format wersjowania wydań

--- application/server/version.h
+++ application/server/version.h
@@ -15,15 +15,15 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR  = 0;
-	static const long MINOR  = 1;
+	static const long MINOR  = 39;
 	static const long BUILD  = 88;
 	static const long REVISION  = 520;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 338;
-	#define RC_FILEVERSION 0,1,88,520
-	#define RC_FILEVERSION_STRING "0, 1, 88, 520\0"
-	static const char FULLVERSION_STRING [] = "0.1.88.520";
+	static const long BUILDS_COUNT  = 341;
+	#define RC_FILEVERSION 0,39,88,520
+	#define RC_FILEVERSION_STRING "0, 39, 88, 520\0"
+	static const char FULLVERSION_STRING [] = "0.39.88.520";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 88;
