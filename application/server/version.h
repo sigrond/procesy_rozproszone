8f41f2aa9b0680e0ab9d134fd5b29c1570067853
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-01-05 17:38:14 +0100

    server LOTC - wyświetlanie pomocy na argument -h . #10

--- application/server/version.h
+++ application/server/version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 1;
-	static const long BUILD  = 2;
-	static const long REVISION  = 3;
+	static const long BUILD  = 3;
+	static const long REVISION  = 6;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 379;
-	#define RC_FILEVERSION 1,1,2,3
-	#define RC_FILEVERSION_STRING "1, 1, 2, 3\0"
-	static const char FULLVERSION_STRING [] = "1.1.2.3";
+	static const long BUILDS_COUNT  = 380;
+	#define RC_FILEVERSION 1,1,3,6
+	#define RC_FILEVERSION_STRING "1, 1, 3, 6\0"
+	static const char FULLVERSION_STRING [] = "1.1.3.6";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 12;
+	static const long BUILD_HISTORY  = 13;
 	
 
 }
