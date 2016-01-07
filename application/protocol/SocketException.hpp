2c06c93009e68d87184bea13be01ccc64cc650d2
anrog <andrzej@localhost.localdomain>
2016-01-07 21:08:09 +0100

    Dokumentacja wyjątków SockEx

--- application/protocol/SocketException.hpp
+++ application/protocol/SocketException.hpp
@@ -15,105 +15,158 @@
 
 #include <string>
 
+/**
+ * \brief Abstrakcyjna klasa bazowa wyjątków związanych z socketami
+ */
 class SockEx : public std::exception
 {
 public:
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept = 0;
 	virtual ~SockEx();
 };
 
-
-
+/**
+ * \brief Wyjątek: nieudane bind()
+ */
 class BindSockEx : public SockEx
 {
 public:
 	BindSockEx();
 	virtual ~BindSockEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 
 private:
 	std::string error;
 };
 
-
-
+/**
+ * \brief Wyjątek: nieudane accept()
+ */
 class AcceptSockEx : public SockEx
 {
 public:
 	AcceptSockEx();
 	virtual ~AcceptSockEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 
 private:
 	std::string error;
 };
 
-
-
+/**
+ * \brief Wyjątek: nieudane socket()
+ */
 class CreateSockEx : public SockEx
 {
 public:
 	CreateSockEx();
 	virtual ~CreateSockEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 
 private:
 	std::string error;
 };
 
-
-
+/**
+ * \brief Wyjątek: nieudane listen()
+ */
 class ListenSockEx : public SockEx
 {
 public:
 	ListenSockEx();
 	virtual ~ListenSockEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 
 private:
 	std::string error;
 };
 
-
-
+/**
+ * \brief Wyjątek: nieudane connect()
+ */
 class ConnectSockEx : public SockEx
 {
 public:
 	ConnectSockEx();
 	virtual ~ConnectSockEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 
 private:
 	std::string error;
 };
 
+/**
+ * \brief Wyjątek: nieudane send()
+ */
 class SendSockEx : public SockEx
 {
 public:
 	SendSockEx();
 	virtual ~SendSockEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 
 private:
 	std::string error;
 };
 
+/**
+ * \brief Wyjątek: nieudane receive()
+ */
 class RecSockEx : public SockEx
 {
 public:
 	RecSockEx();
 	virtual ~RecSockEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 
 private:
 	std::string error;
 };
 
-class TimeoutEx : public SockEx
+/**
+ * \brief Wyjątek: timeout na receive()
+ */
+class TimeoutEx : public RecSockEx
 {
 public:
 	TimeoutEx();
 	virtual ~TimeoutEx();
+	/**
+	 * \brief  zwraca zakończony \0 komunikat o błędzie
+	 * \return wzkaźnik na bufor z komunikatem
+	 */
 	virtual const char* what() const noexcept;
 private:
 	std::string error;
